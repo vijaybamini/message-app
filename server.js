3b60c8ea2eb3cca66e525465c82f7d193cff9834
vijaybamini <vijaykumarbamini@gmail.com>
2026-02-08 17:49:53 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve your static HTML/CSS/JS files
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    // Listen for chat messages
+    socket.on('chatMessage', (msg) => {
+        // Broadcast to everyone else (Simple 1-to-1 logic)
+        socket.broadcast.emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
